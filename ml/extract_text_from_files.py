08d901df64ac55c3e8706ca5da9f1662e5d0ade9
Cord Thomas <cord.thomas@gmail.com>
2022-03-24 17:51:33 -0700

    check if txt file already processed

--- ml/extract_text_from_files.py
+++ ml/extract_text_from_files.py
@@ -1,8 +1,9 @@
 import PIL
 import pytesseract
 import pdf2image
+from os import remove
 from os import walk
-from os.path import isfile, isdir, join
+from os.path import exists, isfile, isdir, join
 from pdf2image import pdfinfo_from_path, convert_from_path
 
 base_path = '/mnt/usb1/srv/projects/city-council-files/documents/'
@@ -15,25 +16,34 @@ def extract(base_path, years):
     files_processed = 0
     year_path = base_path + year
     for root, subdirs, files in walk(year_path):
-      for file in files:
-        file_path = join(root, file)
-        print(file_path)
-        info=pdfinfo_from_path(file_path)
-        maxPages = info["Pages"]
-        pages = convert_from_path(file_path, first_page=0, last_page = min(9,maxPages))
-        page_idx = 0
-        for page in pages:
-          image_file = file_path[:-4] + '-' + str(page_idx) + '.tiff'
-          page_idx += 1
-          page.save(image_file)
-          txt_file = file_path[:-4] + '-' + str(page_idx) + '.txt'
-          string_contents = pytesseract.image_to_string(image_file)
-          with open (txt_file, 'w') as f:
-            f.write(string_contents)
-        files_processed += 1
+      if not ('13-0516' in root or '13-0318' in root or '13-1122' in root or '13-0343' in root):
+        for file in files:
+          file_path = join(root, file)
+          if exists(file_path[:-4] + '.txt'): 
+            print('   skipping, already processed')
+          else:
+            try:
+              info=pdfinfo_from_path(file_path)
+              maxPages = info["Pages"]
+              print('{}: {}'.format(str(maxPages), file_path))
+              pages = convert_from_path(file_path, first_page=0, last_page = min(9,maxPages))
+              page_idx = 0
+              final_file_path = file_path[:-4] + '.txt'
+              with open (final_file_path, 'w') as final_extract:
+                for page in pages:
+                  image_file = file_path[:-4] + '-' + str(page_idx) + '.tiff'
+                  page.save(image_file)
+                  string_contents = pytesseract.image_to_string(image_file)
+                  final_extract.write(string_contents)
 
-        if files_processed >= 50:
-          exit()
+                  remove(image_file)
+                  page_idx += 1
+                  files_processed += 1
+
+                  # if files_processed >= 50:
+                  #   exit()
+            except Exception as e:
+              print(e)
 
 if __name__ == "__main__":
 
